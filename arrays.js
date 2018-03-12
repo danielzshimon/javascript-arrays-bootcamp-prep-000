566283c535d42651dd0b8d96aedaf0793aa7575d
Daniel Shimon <danielzshimon@gmail.com>
2018-03-12 15:43:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,19 @@
 var chocolateBars = ['snickers, hundred grand, kitkat, skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  for(i= 0; i< array.length; i++){
+  for(array= 0; array < array.length; array++){
   return ['element',...array];
   }    
 }
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  for(array= 0; array < array.length; array++){
   array.unshift(element);
+
+  }
 }
+
 
 function addElementToEndOfArray(){
   
